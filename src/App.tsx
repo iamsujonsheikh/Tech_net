bfe9a4418f934e2391650e92c67aa3ae7d8be478
sujonphero <sujonsheikh.in@gmail.com>
2023-07-23 11:08:07 +0600

    done initialize

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-center py-10 text-3xl text-purple-400 font-bold">Hello there...</h1>
+    </div>
+  )
+}
+
+export default App
